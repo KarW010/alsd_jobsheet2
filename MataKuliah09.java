2ad5031e975cb59485f63eb7a86b6db1ad784911
KarW010 <kartikostudy@gmail.com>
2025-02-18 16:31:36 +0700

    Praktikum1

---
+++ MataKuliah09.java
@@ -0,0 +1,40 @@
+public class MataKuliah09 {
+    String kodeMK;
+    String nama;
+    int sks;
+    int jumlahJam;
+
+    void tampiliniInformasi(){
+        System.out.println("Kode MK: " + kodeMK);
+        System.out.println("Nama: " + nama);
+        System.out.println("SKS: " + sks);
+        System.out.println("Jumlah Jam: " + jumlahJam);
+    }
+
+    void ubahSKS(int sksBaru){
+        sks = sksBaru;
+    }
+
+    void tambahJam(int jam){
+        jumlahJam += jam;
+    }
+
+    void kurangiJam(int jam){
+        if (jumlahJam > jam){
+            jumlahJam -= jam;
+        } else {
+            System.out.println("Pengurangan tidak dapat dilakukan");
+        }
+    }
+
+    public MataKuliah09(){
+
+    }
+
+    public MataKuliah09(String kode, String nm, int sks, int juJam){
+        kodeMK = kode;
+        nama = nm;
+        this.sks = sks;
+        jumlahJam = juJam;
+    }
+}
---
+++ MataKuliahMain09.java
@@ -0,0 +1,20 @@
+public class MataKuliahMain09 {
+    public static void main(String[] args) {
+        MataKuliah09 mk1 = new MataKuliah09();
+        mk1.kodeMK = "SIB242004";
+        mk1.nama = "Algoritma Dan Struktur Data";
+        mk1.sks = 2;
+        mk1.jumlahJam = 4;
+
+        mk1.tampiliniInformasi();
+        mk1.tambahJam(2);
+        mk1.ubahSKS(3);
+        mk1.tampiliniInformasi();
+
+        MataKuliah09 mk2 = new MataKuliah09("SIB242005", "Praktikum Algoritma dan Struktur Data", 2, 6);
+        mk2.tampiliniInformasi();
+        mk2.kurangiJam(7);
+        mk2.tampiliniInformasi();
+
+    }
+}
